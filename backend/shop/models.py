dda8620c9e74a30f13a7f1d9407ed5eddd741ab0
unknown <ufukogutcu@gmail.com>
2023-10-04 11:42:12 +0100

    Item model

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image = models.ImageField(default='default.png')
+
+    def __str__(self):
+        return self.name
